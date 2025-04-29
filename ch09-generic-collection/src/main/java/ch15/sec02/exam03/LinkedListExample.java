728f4e5cc15f75ef911c5f8b374ef5a8923375c2
M6F\student <sojsnake@gmail.com>
2025-04-29 17:39:45 +0900

    04/29

---
+++ ch09-generic-collection/src/main/java/ch15/sec02/exam03/LinkedListExample.java
@@ -0,0 +1,29 @@
+package ch15.sec02.exam03;
+
+import java.util.*;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        List<Board> list = new LinkedList<>();
+
+        for(int i=1; i<6; i++){
+            Board b = new Board("제목"+i, "내용"+i, "작가"+i);
+            list.add(b);
+        }
+        System.out.println("요소 개수: "+list.size());
+        System.out.println();
+        System.out.println("3번째 데이터: "+list.get(2));
+        System.out.println();
+
+
+        for(int i=0; i<list.size(); i++){
+            System.out.println((i+1)+"번째 데이터: "+list.get(i));
+        }
+
+        System.out.println();
+        list.remove(2);
+        for(Board b : list){
+            System.out.println(b);
+        }
+    }
+}
